a8fc8a2e60917707def2adda7fd5380a86a7b655
Oh-hyeon Choung <ohhyeon.choung@gmail.com>
2023-08-09 12:16:42 +0200

    refactor plotter functions

--- src/synutils/plotters.py
+++ src/synutils/plotters.py
@@ -8,25 +8,21 @@ def plot_projections(
     axes: np.ndarray, cls_label: Optional[np.ndarray] = None
 ) -> plt.Figure:
     """plot projected axes of the datapoints
-    each column is plotted to a subplot.
+    each column is plotted to a subplot, versus the first column.
 
     Args:
-        val (np.ndarray): vector or arraylike, each column is plotted to a subplot.
+        axes (np.ndarray): vector or arraylike, each column is plotted to a subplot.
         cls_label (np.ndarray, optional): same shape as val array. class labels per row, defaults to None.
 
     Returns:
         plt.Figure: plot figure
     """
-    fig = plt.figure(figsize=(10, 5))
-    ax = fig.add_subplot(121)
-    ax.scatter(axes[:, 0], axes[:, 1], c=cls_label)
-    ax.set_xlabel("PC1")
-    ax.set_ylabel("PC2")
-    ax.set_title("PC1 vs PC2")
-    ax = fig.add_subplot(122)
-    ax.scatter(axes[:, 0], axes[:, 2], c=cls_label)
-    ax.set_xlabel("PC1")
-    ax.set_ylabel("PC3")
-    ax.set_title("PC1 vs PC3")
+    fig = plt.figure(figsize=(3 * (axes.shape[1] - 1), 3))
+    for ii in range(1, axes.shape[1]):
+        ax = fig.add_subplot(1, axes.shape[1] - 1, ii)
+        ax.scatter(axes[:, 0], axes[:, ii], c=cls_label)
+        ax.set_xlabel("axis 1")
+        ax.set_ylabel(f"axis {ii+1}")
+        ax.set_title(f"axis 1 vs axis {ii+1}")
     plt.tight_layout()
     return fig
