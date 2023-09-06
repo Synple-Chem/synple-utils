4308cef59b64632b50bfa91d56233745defb3632
Oh-hyeon Choung <ohhyeon.choung@gmail.com>
2023-09-06 14:38:49 +0200

    change the logging level

--- src/synutils/helpers.py
+++ src/synutils/helpers.py
@@ -9,14 +9,13 @@ LOGGER = get_logger()
 
 
 def timeit(func):
-    # TODO: change print to logger
     @wraps(func)
     def timeit_wrapper(*args, **kwargs):
         start_time = time.perf_counter()
         result = func(*args, **kwargs)
         end_time = time.perf_counter()
         total_time = end_time - start_time
-        LOGGER.info(
+        LOGGER.debug(
             f"Function {func.__name__} {kwargs.keys()} Took {total_time:.4f} seconds = {total_time/60:.1f} mins"
         )
         return result
