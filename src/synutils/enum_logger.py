e746103cdfa074aae29db021a8ceebc10c595cce
Oh-hyeon Choung <ohhyeon.choung@gmail.com>
2024-02-05 10:46:46 +0100

    enable saving log in a file

--- src/synutils/enum_logger.py
+++ src/synutils/enum_logger.py
@@ -1,13 +1,19 @@
 import logging
 import sys
+from pathlib import Path
 
 
-# Idea borrowed from
-# https://github.com/microsoft/DeepSpeed/blob/master/deepspeed/utils/logging.py
-def get_logger(name=None, level=logging.INFO) -> logging.Logger:
+def get_logger(
+    name=None, level=logging.INFO, filename: Path | None = None
+) -> logging.Logger:
     """Returns a logger that is configured as:
     - by default INFO level or higher messages are logged out in STDOUT.
     - format includes file name, line number, etc.
+
+    Args:
+        name (str, optional): Name of the logger. Defaults to None.
+        level (int, optional): Logging level. Defaults to logging.INFO.
+        filename (Path, optional): File to log to. Defaults to None, by default logs to STDOUT.
 
     usage:
         LOGGER = get_logger()
@@ -31,5 +37,11 @@ def get_logger(name=None, level=logging.INFO) -> logging.Logger:
     handler_out = logging.StreamHandler(sys.stdout)
     handler_out.setFormatter(log_formatter)
     logger.addHandler(handler_out)
+
+    # log to filename
+    if filename is not None:
+        file_handler = logging.FileHandler(filename)
+        file_handler.setFormatter(log_formatter)
+        logger.addHandler(file_handler)
 
     return logger
